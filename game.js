bb6035926bb4137329bda5b3eac6f67b3d7cb444
AirWick <erikbolt11@gmail.com>
2021-08-04 20:48:42 -0700

    created one round of rock paper scissors to be played in the console with hardcoded player decision.

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 function computerPlay(){
-    let decisions = ["rock","paper","scissors"];
+    let decisions = ["rock","paper","scissors"]; // all in lower case
     let randomIndex = getRandomInt(0,decisions.length);
     return decisions[randomIndex];
 }
@@ -10,6 +10,50 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-let cpu = computerPlay();
 
-console.log(cpu);
+function playRound(playerSelection, computerSelection){
+
+    if (computerSelection == playerSelection)
+    {
+        return "It's a Tie!";
+    }
+
+    if (computerSelection.toLowerCase() == "rock" && playerSelection == "paper") {
+        return "Player Wins! Paper beats Rock.";
+    }
+
+    if (computerSelection.toLowerCase() == "rock" && playerSelection == "scissors")
+    {
+        return "Computer Wins! Rock beats Scissors.";
+    }
+
+    if (computerSelection.toLowerCase() == "paper" && playerSelection == "scissors")
+    {
+        return "Player Wins! Scissors beats Paper.";
+    }
+
+    if (computerSelection.toLowerCase() == "paper" && playerSelection == "rock")
+    {
+        return "Computer Wins! Paper beats Rock.";
+    }
+
+    if (computerSelection.toLowerCase() == "scissors" && playerSelection == "rock")
+    {
+        return "Player Wins! Rock beats Scissors.";
+    }
+
+    if (computerSelection.toLowerCase() == "scissors" && playerSelection == "paper")
+    {
+        return "Computer Wins! Scissors beats Paper.";
+    }
+}
+
+const playerSelection = "scissors";
+const computerSelection = computerPlay();
+
+console.log("player: " + playerSelection);
+console.log("computer: " + computerSelection);
+
+let round = playRound(playerSelection,computerSelection);
+
+console.log(round);
